590e8c19711bae2c5de679a4d94b7f3016145672
Max Pijittum <mpijittum@fb.com>
2020-06-06 02:27:04 +0800

    initial version

---
+++ web/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/components/index.tsx",
+  target: "web",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename:  "[name].[chunkhash].js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "awesome-typescript-loader",
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader",
+      },
+      {
+        test: /\.css$/,
+        loader: "css-loader",
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { 
+          from: path.resolve(__dirname, 'public', 'favicon.ico'), 
+          to: path.resolve(__dirname, 'build', 'favicon.ico') 
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./src/yourfile.css",
+    }),
+  ],
+};
